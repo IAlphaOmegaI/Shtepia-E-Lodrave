0b18a0bb9a97a3926e49474bb306b531c7135710
Alvi24 <24alvi@gmail.com>
2026-01-14 16:17:45 +0100

    style: update home header

--- src/components/banners/lodra-banner.tsx
+++ src/components/banners/lodra-banner.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LodraBanner = () => {
   return (
-    <div className="relative flex min-h-[510px] sm:min-h-[550px] md:min-h-[700px] lg:min-h-[800px] xl:min-h-[950px] overflow-hidden mb-[50px] bg-[#fff9f0] bg-[url('/home/festiv.jpg')] bg-cover bg-center bg-no-repeat">
+    <div className="relative flex min-h-[510px] sm:min-h-[550px] md:min-h-[700px] lg:min-h-[800px] xl:min-h-[950px] overflow-hidden mb-[50px] bg-[#fff9f0] bg-[url('/home/home-header.jpg')] bg-cover bg-center bg-no-repeat">
       {/* Top Clouds - left-aligned on mobile, centered on larger screens */}
       <div className="absolute top-0 left-0 right-0 w-full h-[141px] z-[1] bg-[url('/home/cloud.svg')] bg-left bg-no-repeat bg-cover sm:bg-center" />
     </div>
